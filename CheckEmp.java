4896cb689ae5045ad4a7aad3bb2d2374aaa2b481
basha <bashamb787@gmail.com>
2022-01-22 15:39:17 +0530

    Add part Time

--- CheckEmp.java
+++ CheckEmp.java
@@ -1,21 +1,28 @@
-public class CheckEmp {
+
+	public class CheckEmp {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program ");
 		int Is_Present=1;
+		int Is_Part_Time=2;
 		int Sal_Per_Hr=20;
 		int Work_Hr;
-		double emp = Math.floor(Math.random() * 10) %  2;
+		double emp = Math.floor(Math.random() * 10) %  3;
 		if (emp == 	Is_Present) {
 		System.out.println("Employee is present");
 		Work_Hr=8;
+		}
+		else if (emp == Is_Part_Time) {
+			System.out.println("Employee is parttimer");
+			Work_Hr=4;
 		}
 		else{
 		System.out.println("Employee is absent");
 		Work_Hr=0;
 	}
 		int Daily_Wage= (Sal_Per_Hr * Work_Hr);
-		System.out.println("salary will be :" +Daily_Wage );
+		System.out.println("salary will be" +Daily_Wage);
+
 	}
 	}
