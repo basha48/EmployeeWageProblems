6f790338d5edbccc9b5132904f8dc34374704aba
basha <bashamb787@gmail.com>
2022-01-22 15:22:22 +0530

    Welcome to wage Computation

---
+++ CheckEmp.java
@@ -0,0 +1,14 @@
+public class CheckEmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int Is_Full_Time=1;
+		double emp = Math.floor(Math.random() * 10) %  2;
+		if (emp == 	Is_Full_Time)
+		System.out.println("Employee is present");
+		else
+		System.out.println("Employee is absent");
+
+	}
+	}
